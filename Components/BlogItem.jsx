d609e02eab59f4246802b4cddef2fc5021ffdde4
lucky <luckymishra26@gmail.com>
2025-01-28 11:53:52 +0530

    make a blogitem page

---
+++ Components/BlogItem.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+import {blog_data} from "@/Assets/assets"
+
+const BlogItem = () => {
+  return (
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+      
+      <Image src={blog_data[0].image} alt='' width={400} height={400} className='border-b border-black' />
+      <p className='ml-5 mt-5  inline-block bg-black  text-white text-sm'>{blog_data[0].category}</p>
+    </div>
+  )
+}
+
+export default BlogItem
